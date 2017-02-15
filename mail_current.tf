e84462d4c6b63f4c43399d57c1266b862caf599e
vladgordey <vladgordey@gmail.com>
2017-02-14 19:29:13 -0500

    Update mail_current.tf

--- mail_current.tf
+++ mail_current.tf
@@ -5,44 +5,44 @@ provider "aws" {
 
 data "aws_availability_zones" "available" {}
 
-module "my_vpc" {
+module "my_vpc_mod"
     source = "github.com/vladgordey/TF_VPC_mod"
 	vpc_cidr = "${var.vpc_cidr}"
 	short_name ="${var.short_name}"
 	az_count ="${var.az_count}"
 }
 
-module "my_sg" {
+module "my_sg_mod" {
     source = "github.com/vladgordey/TF_SecG_mod"
-	vpc_id = "${module.my_vpc.vpc_id}"
+	vpc_id = "${module.my_vpc_mod.vpc_id}"
 	short_name ="${var.short_name}"
 }
 
-module "my_elb" {
+module "my_elb_mod" {
     source = "github.com/vladgordey/TF_ELB_mod"
-	sec_group = "${module.my_sg.elb_sg}"
-	subnets = ["${module.my_vpc.subnets}"]
+	sec_group = "${module.my_sg_mod.elb_sg}"
+	subnets = ["${module.my_vpc_mod.subnets}"]
 }
 
-module "my_asg" {
+module "my_asg_mod" {
      source = "github.com/vladgordey/TF_ASG_mod"
 	 lc_name = "${var.lc_name}"
      ami_id = "${var.ami_id}"
      instance_type = "${var.instance_type}"
      iam_instance_profile = "${var.iam_instance_profile}"
      key_name = "${var.key_name}"
-     security_group = "${module.my_sg.elb_sg}"
+     security_group = "${module.my_sg_mod.elb_sg}"
      user_data = "${var.user_data_file}"
      asg_name = "${var.asg_name}"
      asg_number_of_instances = "${var.asg_number_of_instances}"
      asg_minimum_number_of_instances = "${var.asg_minimum_number_of_instances}"
-     load_balancer_names = "${module.my_elb.elb_id}"
+     load_balancer_names = "${module.my_elb_mod.elb_id}"
      health_check_type = "${var.health_check_type}"
      availability_zones = "${data.aws_availability_zones.available.names[count.index]}"
-     vpc_zone_subnets = ["${module.my_vpc.subnets}"]
+     vpc_zone_subnets = ["${module.my_vpc_mod.subnets}"]
 }
 
-module "my_rds" {
+module "my_rds_mod" {
     source = "github.com/vladgordey/TF_RDS_mod"
 rds_instance_name="${var.short_name}-RDS"
 rds_is_multi_az="${var.rds_is_multi_az}"
@@ -54,9 +54,9 @@ rds_instance_class="${var.rds_instance_class}"
 database_name="${var.database_name}"
 database_user="${var.database_user}"
 database_password="${var.database_password}"
-rds_security_group_id="${var.rds_security_group_id}"
+rds_security_group_id="${module.my_sg_mod.rds_sg}"
 db_parameter_group="${var.db_parameter_group}"
 publicly_accessible="${var.publicly_accessible}"
-subnet_az1="${module.my_vpc.subnet_0}"
-subnet_az2="${module.my_vpc.subnet_1}"
+subnet_az1="${module.my_vpc_mod.subnet_0}"
+subnet_az2="${module.my_vpc_mod.subnet_1}"
 }
